0d0dfb77d56ae11cb60123ad5902e26cd24baadc
Kari Geir Gunnarsson <karigegu@gmail.com>
2018-06-21 00:26:11 +0000

    add data types

---
+++ data-types.js
@@ -0,0 +1,18 @@
+
+let kariGeir = {
+    name: 'Kari',
+    age: 29,
+    favoriteFruits: ['orange','apple', 'banana'],
+    car: {
+        make: 'Ford',
+        model: 'Mustang',
+        isFunctional: true,
+        style: {
+            color: 'purple',
+            texture: 'matte',
+        }
+    }
+
+};
+
+console.log(kariGeir.car.style.color);
--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 console.log('Hello, world');
 console.log('fck yo shit');
+console.log('First', 'Second');
